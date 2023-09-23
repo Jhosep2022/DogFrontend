a8fd025be17173d635aa5fdfc0900c009552c1e0
Jhosep2022 <jose.condori.r@ucb.edu.bo>
2023-09-23 11:13:17 -0400

    manejor de errores y tambien agregacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DogComponent } from '../app/components/dog/dog.component';
 import { DogListComponent } from '../app/components/dog-list/dog-list.component';
-import { AuthGuard } from './authguard';
+import { AuthGuard } from './guards/auth.guard';
+import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
   { path: 'app-dog',
     component: DogComponent,
     canActivate: [AuthGuard],
-    data: { roles: ['user'] }
+    data: { roles: ['USER'] }
   },
-  { path: 'app-dog-list', component: DogListComponent },
-  { path: '', redirectTo: '/app-dog', pathMatch: 'full' },
+  { path: 'app-dog-list',
+    component: DogListComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ADMIN'] }
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  { path: '',
+    redirectTo: '/app-dog',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
